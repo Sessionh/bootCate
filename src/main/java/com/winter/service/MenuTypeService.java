66cc2ec39d1d6b5500320305f873bb43a2d87683
huyh <847143363@qq.com>
2019-02-11 11:42:26 +0800

    实现类修改 mapper写

---
+++ src/main/java/com/winter/service/MenuTypeService.java
@@ -0,0 +1,9 @@
+package com.winter.service;
+
+import com.winter.pojo.MenuType;
+
+import java.util.List;
+
+public interface MenuTypeService {
+    List<MenuType> getListMenuType();
+}
---
+++ src/main/java/com/winter/service/impl/MenuTypeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.winter.service.impl;
+
+import com.winter.pojo.MenuType;
+import com.winter.service.MenuTypeService;
+
+import java.util.List;
+
+public class MenuTypeServiceImpl  implements MenuTypeService {
+    @Override
+    public List<MenuType> getListMenuType() {
+        return null;
+    }
+}
